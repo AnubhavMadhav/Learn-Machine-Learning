7fc564f020f7cf608fb6b6bd60ae0c6ac76c16fd
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-03 16:32:16 +0530

    Importing Libraries Done

---
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -0,0 +1,8 @@
+# Data Preprocessing
+
+# Importing the Libraries
+import numpy as np                  # for linear and vector and matrix calculation
+import matplotlib.pyplot as plt     # for plotting graphs and bars, for data visualization
+import pandas as pd                 # best for importing dataset
+
+
