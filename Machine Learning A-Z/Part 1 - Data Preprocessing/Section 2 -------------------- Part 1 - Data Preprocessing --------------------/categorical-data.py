b20515ab7bcc30057cbfbf265dcea859a966a006
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-04 02:25:09 +0530

    Splitted Dataset into Training and Test Sets

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical-data.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical-data.py
@@ -45,4 +45,4 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train.reshape(-1,1))f
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))
--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -40,5 +40,11 @@ X = ct.fit_transform(X)
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
+# Splitting the Dataset into Training Set and Test Set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state = 0)
+
+
 print(X)
 print(Y)
