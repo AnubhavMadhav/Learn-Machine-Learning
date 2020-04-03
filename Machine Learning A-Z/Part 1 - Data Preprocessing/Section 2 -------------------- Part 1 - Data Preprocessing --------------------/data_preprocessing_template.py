330c74cf68699ebc5b7ab8c30da586f09a150565
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-03 17:20:26 +0530

    Importing the Dataset Done

--- Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -1,8 +1,15 @@
 # Data Preprocessing
 
-# Importing the Libraries
-import numpy as np                  # for linear and vector and matrix calculation
-import matplotlib.pyplot as plt     # for plotting graphs and bars, for data visualization
-import pandas as pd                 # best for importing dataset
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Importing the dataset
+dataset = pd.read_csv("D:\GitHub_Repositories\Learn-Machine-Learning\Machine Learning A-Z\Part 1 - Data Preprocessing\Section 2 -------------------- Part 1 - Data Preprocessing --------------------\Data.csv")
 
+X = dataset.iloc[:,:-1].values              # For Independent Columns              # ':' before ',' represents 'all-rows' and ':-1' right to ',' represents 'all columns except last column'
+Y = dataset.iloc[:, 3].values               # For Dependent Column (Last Col in Data.csv)                   ':' before ',' represents 'all-rows' and  '3' right to ',' represents the index of the last column
+
+print(X)
+print(Y)
