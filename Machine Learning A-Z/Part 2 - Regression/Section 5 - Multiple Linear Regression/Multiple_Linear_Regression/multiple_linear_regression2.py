46dd0f805b91423371bb9bbba058a58bbc9f10d9
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-05 03:47:49 +0530

    Predicting the Test Set Results for Multiple Linear Regression Done

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression2.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv("D:\GitHub_Repositories\Learn-Machine-Learning\Machine Learning A-Z\Part 2 - Regression\Section 5 - Multiple Linear Regression\Multiple_Linear_Regression\Startups.csv")
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 4].values
 
@@ -36,3 +36,13 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+# Fitting the Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+
+# Predicting the Test Set Results
+Y_pred = regressor.predict(X_test)                  # Creating a Vector
