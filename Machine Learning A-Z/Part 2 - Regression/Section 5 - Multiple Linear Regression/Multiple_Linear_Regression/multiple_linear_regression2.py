1823913d42da8558539f5f769288ef339d0ddf0a
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-05 05:55:20 +0530

    Multiple Linear Regression Completed

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression2.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression2.py
@@ -46,3 +46,51 @@ regressor.fit(X_train, Y_train)
 
 # Predicting the Test Set Results
 Y_pred = regressor.predict(X_test)                  # Creating a Vector
+
+
+# Building the optimal model using Backward Elimination
+# Till above, we used all the columns/variables in our model, but there might be some variables/columns which are of no use predict are dependent variable, i.e. some columns/variable do not affects deppendent variable/column much.
+# To, ignore and not use those unnecessary variables/columns in our model, we will use Backward Elimination (we can use other methods too like forward selection or bidirectional elimination but Backward Elimination is faster than rest)
+# We want only those Independent variables/columns in 'X' those who have High Impact on Profit(Dependent Variable)
+# Multiple Linear Regression Model: y = b0 + b1*x1 + b2*x2 + ...... + bn*xn
+# The library which we are going to use do not counts the constant(b0) because it is not associated with any column/variable, but we need that constant otherwise results will ne wrong.
+# To make our library count that constant(b0), we will associate it with x0, where x0=1.
+# We need to make it something like: y = b0*x0 + b1*x1 + b2*x2 + ...... + bn*xn, where x0=1
+# Therefore, we will add a new column/variable 'x0' to our 'X'(Independent Variable) using numpy.
+
+
+# Backward Elimination: Starting with all the Independent Variables in 'X' , we will keep on removing all those independent variables who are statistically insignificant
+
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+# 'axis = 0' - adds a row
+# 'axis = 1' - adds a column
+# Here, we are adding 'X' to Array of ones() because we need ones() at the beginning of 'X'. Therefore, we are not adding ones() to 'X'.
+
+
+# Very Very Important and Updated Part
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_opt = X_opt.astype(int)               # Converted fron object type to int type, otherwise OLS does not works on object type
+# Ordinary Least Squares
+regressor_OLS = sm.OLS(Y, X_opt).fit()
+# Significance Level = 5% i.e. 0.05
+regressor_OLS.summary()             # Reading from the summary, observe the highest p-value among all columns
+# consider the predictor with the highest p-value, if p-value>SL, remove that predictor/variable else go to FInish(Our Model is Ready)
+
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+X_opt = X_opt.astype(int)
+regressor_OLS = sm.OLS(Y, X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3, 5]]
+X_opt = X_opt.astype(int)
+regressor_OLS = sm.OLS(Y, X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0, 3]]
+X_opt = X_opt.astype(int)
+regressor_OLS = sm.OLS(Y, X_opt).fit()
+regressor_OLS.summary()
+
